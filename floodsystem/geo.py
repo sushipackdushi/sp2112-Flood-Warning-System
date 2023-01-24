007545c76314377c30a8fe40c002f7f9081747f2
Sushant Patil <sushipackdushi92@gmail.com>
2023-01-24 20:36:05 +0000

    Task 1B trial attempt

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,16 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+
+def stations_by_distance(stations, p):
+    station_distance = []
+    for i in stations:
+        distance = haversine(i.coord, p)
+        station_distance.append((i, distance))
+
+    sorted_by_key(station_distance, 1)
+
+    return station_distance
+
