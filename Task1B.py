9b106d5aa1b4f1e49c5925bbda6d11ee97adc175
Sushant Patil <sushipackdushi92@gmail.com>
2023-01-26 16:16:29 +0000

    Task 1B completed (working)

--- Task1B.py
+++ Task1B.py
@@ -1,23 +1,20 @@
 from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
 
 
-stations_by_distance([('Cambridge Jesus Lock', 'Cambridge', 0.8402364350834995),
-                      ('Bin Brook', 'Cambridge', 2.502274086951454),
-                      ("Cambridge Byron's Pool", 'Grantchester', 4.0720438555077125),
-                      ('Cambridge Baits Bite', 'Milton', 5.115589516578674),
-                      ('Girton', 'Girton', 5.227070345811418),
-                      ('Haslingfield Burnt Mill', 'Haslingfield', 7.044388165868453),
-                      ('Oakington', 'Oakington', 7.128249171700346), ('Stapleford', 'Stapleford', 7.265694306995238),
-                      ('Comberton', 'Comberton', 7.7350743760373675),
-                      ('Dernford', 'Great Shelford', 7.993861351711722)], (52.2053, 0.1218))
+def run():
+    stations = build_station_list()
+    x = stations_by_distance(stations, (52.2053, 0.1218))
+    a = []
+    for i in x:
+        a.append([(i[0].river, i[0].town, i[1])])
 
-stations_by_distance([('Boscadjack', 'Wendron', 440.0026482838576), ('Gwithian', 'Gwithian', 442.05491558132354),
-                      ('Helston County Bridge', 'Helston', 443.37824966454974),
-                      ('Loe Pool', 'Helston', 445.07184458260684), ('Relubbus', 'Relubbus', 448.64944322554413),
-                      ('St Erth', 'St Erth', 449.03415711886015),
-                      ('St Ives Consols Farm', 'St Ives', 450.0734690482922),
-                      ('Penzance Tesco', 'Penzance', 456.3857579793324),
-                      ('Penzance Alverton', 'Penzance', 458.5766422710278),
-                      ('Penberth', 'Penberth', 467.53367291629183)], (52.2053, 0.1218))
+    closest_stations = [a[:9]]
+    furthest_stations = [a[-10:]]
+    print(f"Closest Stations: {closest_stations}")
+    print(f"Furthest Stations: {furthest_stations}")
 
 
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,13 +10,15 @@ from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
 
 
+# Function for Task 1B
 def stations_by_distance(stations, p):
+    """ ENTER SOMETHING HERE  """
     station_distance = []
     for i in stations:
         distance = haversine(i.coord, p)
         station_distance.append((i, distance))
 
-    sorted_by_key(station_distance, 1)
+    x = sorted_by_key(station_distance, 1)
 
-    return station_distance
+    return x
 
