e3408bf48bd89fc0661bf2b5656112145abe2c1e
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-01-28 14:51:25 +0000

    changing i to station

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -14,11 +14,11 @@ from haversine import haversine, Unit
 def stations_by_distance(stations, p):
     """ ENTER SOMETHING HERE  """
     station_distance = []
-    for i in stations:
-        distance = haversine(i.coord, p)
-        station_distance.append((i, distance))
+
+    for station in stations:
+        distance = haversine(station.coord, p)
+        station_distance.append((station, distance))
 
     x = sorted_by_key(station_distance, 1)
 
     return x
-
