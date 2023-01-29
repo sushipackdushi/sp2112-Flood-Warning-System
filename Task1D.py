ce9041e144caab276a41efa9b9d9879e44747b3c
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-01-29 00:39:40 +0000

    part 1 of task 1D (formatting)

--- Task1D.py
+++ Task1D.py
@@ -1,12 +1,14 @@
 from floodsystem.geo import rivers_with_station
 from floodsystem.stationdata import build_station_list
 
+
 def river_list():
     stations = build_station_list()
     rivers = rivers_with_station(stations)
     x = sorted(rivers)
     print("number of rivers with at least one monitoring station:", len(rivers))
     print(x[:10])
+
 
 if __name__ == "__main__":
     print()
