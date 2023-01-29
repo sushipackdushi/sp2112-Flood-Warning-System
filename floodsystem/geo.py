593ed85a378607d75d0a7f7afbc8e451321506ba
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-01-29 15:00:05 +0000

    part 2 of task 1D (geo + task 1D) - completed

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -2,9 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
-geographical data.
-
-"""
+geographical data."""
 
 from .utils import sorted_by_key  #noqa
 from haversine import haversine #Unit
@@ -23,7 +21,8 @@ def stations_by_distance(stations, p):
 
     return x
 
-# function for task 1D
+
+# function for task 1D part 1
 def rivers_with_station(stations):
 
     list_of_rivers = []
@@ -33,3 +32,20 @@ def rivers_with_station(stations):
         list_of_rivers.append(river)
 
     return set(list_of_rivers)
+
+
+# function for task 1D part 2
+def stations_by_river(stations):
+
+    dict_stations_by_river = {}
+
+    for station in stations:
+        river = station.river
+        if river in dict_stations_by_river:
+            existing_list = dict_stations_by_river[river]
+            existing_list.append(station)
+            dict_stations_by_river[river] = existing_list
+        else:
+            dict_stations_by_river[river] = [station]
+
+    return dict_stations_by_river
