da98f180cc330c8431e391a474df702abac12bbf
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-02-05 18:04:25 +0000

    test for 1E

---
+++ test_task1E.py
@@ -0,0 +1,19 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+stations = build_station_list()
+
+print()
+print("Test for 1E:")
+
+
+def test_for_1e():
+    if len(rivers_by_station_number(stations,9)) >= 9:
+        return True, print("No errors found")
+    else:
+        return False, print("Error found")
+
+
+test_for_1e()
+
+
