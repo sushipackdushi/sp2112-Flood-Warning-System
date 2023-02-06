1f9cd81ae25b5b13cf1f64277a3a2b3a43a92035
Sushant Patil <sushipackdushi92@gmail.com>
2023-02-06 10:36:52 +0000

    test for task 1B updated

--- test_Task1A.py
+++ test_Task1A.py
@@ -1,15 +1,17 @@
 from haversine import haversine
 from floodsystem.utils import sorted_by_key  #noqa
+from stationdata import build_station_list
 
 
-def test_stations_by_distance(stations, p):
+def test_stations_by_distance():
     """ Returns a sorted list of (station, distance) tuples, where distance (float) is the distance of the station
     (MonitoringStation) from the coordinate p  """
 
+    x = build_station_list()
     station_distance = []
 
-    for station in stations:
-        distance = haversine(station.coord, p)
+    for station in x:
+        distance = haversine(station.coord, (0, 0))
         station_distance.append((station, distance))
 
     x = sorted_by_key(station_distance, 1)
