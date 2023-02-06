2337373730bfe4263b7bbc7f42773874e685a8f6
Sushant Patil <sushipackdushi92@gmail.com>
2023-02-06 10:46:12 +0000

    test for task 1B updated

--- test_Task1B.py
+++ test_Task1B.py
@@ -17,5 +17,5 @@ def test_stations_by_distance():
 
     x = geo.stations_by_distance(test_stations, (52.2053, 0.1218))
 
-    assert x == [('station1', 157.2495984740402), ('station2', 314.47523947196964), ('station3', 417.65293997288967)]
+    assert x == [('station1', 157.2495984740402), ('station2', 314.47523947196964), ('station3', 471.65293997288967)]
 
