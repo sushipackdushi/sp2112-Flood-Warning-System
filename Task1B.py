31afc7a0d5c19bcfb59b1fc10df7c20e9822acb2
Sushant Patil <sushipackdushi92@gmail.com>
2023-02-09 12:28:20 +0000

    final test

--- Task1B.py
+++ Task1B.py
@@ -5,12 +5,10 @@ from floodsystem.stationdata import build_station_list
 def run():
     stations = build_station_list()
     x = stations_by_distance(stations, (52.2053, 0.1218))
-    a = []
-    for i in x:
-        a.append([(i[0].river, i[0].town, i[1])])
+    print(x)
 
-    closest_stations = [a[:9]]
-    furthest_stations = [a[-10:]]
+    closest_stations = [x[:9]]
+    furthest_stations = [x[-10:]]
     print(f"Closest Stations: {closest_stations}")
     print(f"Furthest Stations: {furthest_stations}")
 
