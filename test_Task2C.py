a0c8e6aade3197141e2967f864941c1dd4bd24f2
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-02-23 23:30:40 +0000

    Test for 2C (complete)

--- test_Task2C.py
+++ test_Task2C.py
@@ -1,7 +1,8 @@
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 stations = build_station_list()
+update_water_levels(stations)
 
 
 def test_for_stations_level_over_threshold():
