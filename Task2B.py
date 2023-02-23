fa17cd570e2fefd803f2f8229dd90581381b93d2
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-02-23 20:04:16 +0000

    task 2B complete (final)

--- Task2B.py
+++ Task2B.py
@@ -12,7 +12,12 @@ for station in stations_level_over_threshold(stations, 0.8):
     list.append((name, relative_water_level))
 
 print()
+print("Stations and their relative water level, in descending order:")
 for i in list[:10]:
     print(i)
+
+print()
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
 
 
