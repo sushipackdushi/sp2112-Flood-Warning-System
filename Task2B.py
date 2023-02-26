c9e0556e748b4ddf16403f25a3a657443fd94a64
rqc23 <123369593+rqc23@users.noreply.github.com>
2023-02-26 13:52:30 +0000

    def run for 2B & 2C

--- Task2B.py
+++ Task2B.py
@@ -4,20 +4,25 @@ from floodsystem.flood import stations_level_over_threshold
 stations = build_station_list()
 update_water_levels(stations)
 
-
 list = []
-for station in stations_level_over_threshold(stations, 0.8):
-    name = station[0].name
-    relative_water_level = station[1]
-    list.append((name, relative_water_level))
 
-print()
-print("Stations and their relative water level, in descending order:")
-for i in list[:10]:
-    print(i)
 
-print()
+def run():
+    for station in stations_level_over_threshold(stations, 0.8):
+        name = station[0].name
+        relative_water_level = station[1]
+        list.append((name, relative_water_level))
+
+    print()
+    print("Stations and their relative water level, in descending order:")
+    for i in list[:10]:
+        print(i)
+
+    print()
+
+
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
 
 
--- Task2C.py
+++ Task2C.py
@@ -5,14 +5,22 @@ stations = build_station_list()
 update_water_levels(stations)
 
 list = []
-for station in stations_highest_rel_level(stations, 10):
-    name = station[0].name
-    relative_water_level = station[1]
-    list.append((name, relative_water_level))
 
-print()
-print("Nth highest relative water levels and their stations: ")
-print("N=10")
-print()
-for i in list:
-    print(i)
+
+def run():
+    for station in stations_highest_rel_level(stations, 10):
+        name = station[0].name
+        relative_water_level = station[1]
+        list.append((name, relative_water_level))
+
+    print()
+    print("Nth highest relative water levels and their stations: ")
+    print("N=10")
+    print()
+    for i in list:
+        print(i)
+
+
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    run()
